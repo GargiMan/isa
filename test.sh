697ba69884dc6a08ad8328ed73e9f2019048e093
Marek Gergel <mage26gg@gmail.com>
2023-11-07 15:19:41 +0100

    Updated test script

--- test.sh
+++ test.sh
@@ -8,8 +8,10 @@ dns_addresses="kazi.fit.vutbr.cz 8.8.8.8"
 for address in $addresses; do
     for dns_address in $dns_addresses; do
         echo "dig $address @$dns_address"
-        dig $address @$dns_address
+        dig "$address" @"$dns_address"
         echo "nslookup $address $dns_address"
-        nslookup $address $dns_address
+        nslookup type=CNAME "$address" "$dns_address"
     done
 done
+
+# opts:   type=A/AAAA/CNAME , recurse
